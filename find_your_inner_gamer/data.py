6e670de61fd96ad5d62a58fe011c0ec889b282e4
João Marques <JoaoMarques1@users.noreply.github.com>
2022-03-04 09:06:41 +0000

    creating class trainer for model

--- find_your_inner_gamer/data.py
+++ find_your_inner_gamer/data.py
@@ -1,4 +1,10 @@
-### get main csv
+from distutils.command.clean import clean
+import pandas as pd
+
+def get_data():
+    return pd.read_csv('../raw_data/clean_df.csv')
+
+
 ### merge with review csv + date csv + price csv
 ### replace na by english in languages
 
