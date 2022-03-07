4f42aaf8ebbcf25a9ecf90db39aa4de666efb736
Luis Queiros <luis.cap.queiros@gmail.com>
2022-03-07 15:06:07 +0000

    synch

--- find_your_inner_gamer/data.py
+++ find_your_inner_gamer/data.py
@@ -1,17 +1,31 @@
 import pandas as pd
 from google.cloud import storage
-from find_your_inner_gamer.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH
+from find_your_inner_gamer.params import BUCKET_NAME, BUCKET_CSV_DATA_PATH,BUCKET_TRANSFORMED_CSV_DATA_PATH,MODEL_NAME, MODEL_VERSION
 
 
 def get_data():
     return pd.read_csv('../raw_data/clean_df.csv' )
 
-def get_data_from_gcp():
+def get_clean_data_from_gcp():
     """method to get the  data from google cloud bucket"""
     # Add Client() here
     client = storage.Client()
-    path = f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}"
+    path = f"gs://{BUCKET_NAME}/{BUCKET_CSV_DATA_PATH}"
     df = pd.read_csv(path)
     return df
 
+def get_X_data_from_gcp():
+    """method to get the  data from google cloud bucket"""
+    # Add Client() here
+    client = storage.Client()
+    path = f"gs://{BUCKET_NAME}/{BUCKET_TRANSFORMED_CSV_DATA_PATH}"
+    df = pd.read_csv(path)
+    return df
 
+def get_model_from_gcp():
+    """method to get the model from google cloud bucket"""
+    client = storage.Client().bucket(BUCKET_NAME)
+    local_model_name = 'model.joblib'
+    storage_location = f"models/{MODEL_NAME}/{MODEL_VERSION}/{local_model_name}"
+    blob = client.blob(storage_location)
+    blob.load_from_filename('model.joblib')
